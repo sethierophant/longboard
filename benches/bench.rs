c61fe3e244032a2a0f864ca4bf86aa28a4c73648
set <set@hierophant.club>
2020-04-15 17:29:17 -0400

    Run `cargo fmt`

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use rocket::local::{Client, LocalResponse};
 
-use longboard::{Config, new_instance};
+use longboard::{new_instance, Config};
 
 fn get_home<'c>(client: &'c Client) -> LocalResponse<'c> {
     client.get("/").dispatch()
@@ -12,9 +12,7 @@ pub fn bench_homepage(c: &mut Criterion) {
     let rocket = new_instance(Config::default()).unwrap();
     let client = Client::new(rocket).expect("valid rocket instance");
 
-    c.bench_function("home", |b| b.iter(|| {
-        get_home(black_box(&client))
-    }));
+    c.bench_function("home", |b| b.iter(|| get_home(black_box(&client))));
 }
 
 criterion_group!(benches, bench_homepage);
